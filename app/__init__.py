13f84339d41121f6c4ebefb9397ed45f73463b25
Miguel <mamanriquer@unal.edu.co>
2023-12-08 16:58:06 -0500

    Controllers

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from ibapi.client import *
+from ibapi.common import BarData
 from ibapi.wrapper import *
 
-import logging
-import os
+# import logging
+# import os
 
-logging.basicConfig(
-    filename= os.path.join('..', 'debbug_app.log'),
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+# logging.basicConfig(
+#     filename= os.path.join('..', 'debbug_app.log'),
+#     level=logging.DEBUG,
+#     format='%(asctime)s - %(levelname)s - %(message)s'
+# )
 
 class Wrapper(EWrapper):
     def __init__(self):
@@ -19,15 +20,39 @@ class Client(EClient):
         EClient.__init__(self,wrapper)
 
 class App(Wrapper,Client):
+    _instance = None
     def __init__(self):
         Wrapper.__init__(self)
         Client.__init__(self,wrapper=self)
     
-    def contractDetails(self, reqId: int, contractDetails: ContractDetails):
-        logging.info(f"contract details: {contractDetails}")
-        print(f"contract details: {contractDetails}")
+    # def contractDetails(self, reqId: int, contractDetails: ContractDetails):
+    #     # logging.info(f"contract details: {contractDetails}")
+    #     print(f"contract details: {contractDetails}")
 
-    def contractDetailsEnd(self, reqId: int):
-        logging.info("End of contractDetails")
-        print("End of contractDetails")
+    # def contractDetailsEnd(self, reqId: int):
+    #     # logging.info("End of contractDetails")
+    #     print("End of contractDetails")
+    #     self.disconnect()
+
+
+    def nextValidId(self, orderId: int):
+        mycontract = Contract()
+        mycontract.symbol = "AAPL"
+        mycontract.secType = "STK"
+        mycontract.exchange = "SMART"
+        mycontract.currency = "USD"
+        self.reqHistoricalData(orderId, mycontract, "20221010 15:00:00 US/Eastern", "1 D", "1 hour", "TRADES", 0, 1, 0, [])
+
+    def historicalData(self, reqId: int, bar: BarData):
+        print("starting historical data")
+        print(bar)
+    
+    def historicalDataEnd(self, reqId: int, start: str, end: str):
+        print("End of historical Data")
+        print(reqId, start, end)
         self.disconnect()
+
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
--- app/main.py
+++ app/main.py
@@ -1,21 +1,32 @@
 from ibapi.contract import Contract
+# from controller.market_data.ibHistMktData import IBHistMktData
 
 from __init__ import App
 
 def main():
 
     app = App()
-
     app.connect("127.0.0.1",7497,1000)
+    app.run()
+
 
     contract = Contract()
     contract.symbol = "AAPL"
     contract.secType = "STK"
     contract.exchange = "SMART"
     contract.currency = "USD"
+    start_date = "20231208 16:00:00 US/Eastern"
+    broll_dates = "1 D"
+    freq = "5 min"
+    on = "TRADES"
+    orderID = 1
+    
 
-    app.reqContractDetails(1,contract)
-    app.run()
+    app.getData()
+
+    print("done")
+
+    
 
 
 if __name__ == "__main__":
