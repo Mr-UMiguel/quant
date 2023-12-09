bb378b3d641fac96016f39811153204b6c0fd0d5
Miguel <mamanriquer@unal.edu.co>
2023-12-09 14:23:30 -0500

    Understanding the API

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from ibapi.client import *
-from ibapi.common import BarData
+from ibapi.common import BarData, Decimal, OrderId, TickerId
+from ibapi.contract import Contract
+from ibapi.order import Order
+from ibapi.order_state import OrderState
+from ibapi.utils import Decimal
 from ibapi.wrapper import *
 
+import json
 # import logging
 # import os
 
@@ -12,19 +17,88 @@ from ibapi.wrapper import *
 # )
 
 class Wrapper(EWrapper):
+
     def __init__(self):
         EWrapper.__init__(self)
+
+    def error(self, reqId: TickerId, errorCode: int, errorString: str, advancedOrderRejectJson=""):
+        print("***")
+        return super().error(reqId, errorCode, errorString, advancedOrderRejectJson)
+
+    def nextValidId(self, orderId: int):
+        print("+++")
+        print(orderId)
+        return super().nextValidId(orderId)
+    
+
+    def accountSummary(self, reqId: int, account: str, tag: str, value: str, currency: str):
+        print("---AccountSummary---")
+        print(reqId, account, tag, value, currency)
+        self.disconnect()
+
+    
+    def openOrder(self, orderId: OrderId, contract: Contract, order: Order, orderState: OrderState):
+        print("---openOrder---")
+        print(orderId, contract, order, orderState)
+        self.disconnect()
+
+
+    def currentTime(self, time: int):
+        print("---currentTime---")
+        print(time)
+        return super().currentTime(time)
+    def pnlSingle(self, reqId: int, pos: Decimal, dailyPnL: float, unrealizedPnL: float, realizedPnL: float, value: float):
+        print("---pnl---")
+        print(reqId, pos, dailyPnL, unrealizedPnL, realizedPnL, value)
+        return super().pnlSingle(reqId, pos, dailyPnL, unrealizedPnL, realizedPnL, value)
+
+    def historicalData(self, reqId: int, bar: BarData):
+        with open('source/testMktData.json', 'r+') as file:
+            data = json.load(file)
+            data.append({
+                'reqid' : str(reqId),
+                'date': bar.date,
+                'open': str(bar.open),
+                'high': str(bar.high),
+                'low' : str(bar.low),
+                'close': str(bar.close),
+                'volume': str(bar.volume),
+                'wap'  : str(bar.wap),
+                'count': str(bar.barCount)
+            })
+
+            file.seek(0)
+            json.dump(data, file, indent=4)
+            file.truncate()
+
+        return super().historicalData(reqId, bar)
+    
+    def historicalDataEnd(self, reqId: int, start: str, end: str):
+        print("---HistoricalDataEnd---")
+        print(f"start:{start}, end:{end}")
+        return super().historicalDataEnd(reqId, start, end)
 
 class Client(EClient):
     def __init__(self,wrapper):
         EClient.__init__(self,wrapper)
+
+    def connect(self, host, port, clientId):
+        print("Connection Established")
+        return super().connect(host, port, clientId)
+    
+    def disconnect(self):
+        print("Connection has been disconnected")
+        return super().disconnect()
+    
+    def l(cls):
+        cls.reqPnLSingle()
 
 class App(Wrapper,Client):
     _instance = None
     def __init__(self):
         Wrapper.__init__(self)
         Client.__init__(self,wrapper=self)
-    
+
     # def contractDetails(self, reqId: int, contractDetails: ContractDetails):
     #     # logging.info(f"contract details: {contractDetails}")
     #     print(f"contract details: {contractDetails}")
@@ -35,24 +109,33 @@ class App(Wrapper,Client):
     #     self.disconnect()
 
 
-    def nextValidId(self, orderId: int):
-        mycontract = Contract()
-        mycontract.symbol = "AAPL"
-        mycontract.secType = "STK"
-        mycontract.exchange = "SMART"
-        mycontract.currency = "USD"
-        self.reqHistoricalData(orderId, mycontract, "20221010 15:00:00 US/Eastern", "1 D", "1 hour", "TRADES", 0, 1, 0, [])
+    # def nextValidId(self, orderId: int):
+    #     mycontract = Contract()
+    #     mycontract.symbol = "AAPL"
+    #     mycontract.secType = "STK"
+    #     mycontract.exchange = "SMART"
+    #     mycontract.currency = "USD"
+    #     self.reqHistoricalData(orderId, mycontract, "20221010 15:00:00 US/Eastern", "1 D", "1 hour", "TRADES", 0, 1, 0, [])
 
-    def historicalData(self, reqId: int, bar: BarData):
-        print("starting historical data")
-        print(bar)
+    # def historicalData(self, reqId: int, bar: BarData):
+    #     print("starting historical data")
+    #     print(bar)
     
-    def historicalDataEnd(self, reqId: int, start: str, end: str):
-        print("End of historical Data")
-        print(reqId, start, end)
-        self.disconnect()
+    # def historicalDataEnd(self, reqId: int, start: str, end: str):
+    #     print("End of historical Data")
+    #     print(reqId, start, end)
+    #     self.disconnect()
+
 
     def __new__(cls):
         if not cls._instance:
             cls._instance = super().__new__(cls)
         return cls._instance
+    
+    def __delattr__(cls) -> None:
+        cls._instance = None
+        if not cls.isConnected():
+            cls.disconnect()
+        
+    
+    
--- app/main.py
+++ app/main.py
@@ -1,31 +1,43 @@
 from ibapi.contract import Contract
-# from controller.market_data.ibHistMktData import IBHistMktData
+from controller.market_data.ibHistMktData import IBHistMktData
 
 from __init__ import App
+
+import json
+import time
+import os
+
+file_path = os.path.join('source', 'testMktData.json')
+with open(file_path, 'w') as file:
+        json.dump([], file, indent=4) 
 
 def main():
 
     app = App()
     app.connect("127.0.0.1",7497,1000)
-    app.run()
 
-
-    contract = Contract()
-    contract.symbol = "AAPL"
-    contract.secType = "STK"
-    contract.exchange = "SMART"
-    contract.currency = "USD"
-    start_date = "20231208 16:00:00 US/Eastern"
-    broll_dates = "1 D"
-    freq = "5 min"
-    on = "TRADES"
-    orderID = 1
     
+    # app.reqAccountSummary(12,groupName="All",tags='TotalCashValue')
 
-    app.getData()
+    mycontract = Contract()
+    mycontract.symbol = "AAPL"
+    mycontract.secType = "STK"
+    mycontract.exchange = "SMART"
+    mycontract.currency = "USD"
 
-    print("done")
+    endDate = '20231208 16:31:00 US/Eastern'
+    bRollOver = '2 D'
+    bSize = '30 mins'
+    wTS = 'TRADES'
 
+    time.sleep(3)
+    app.reqHistoricalData(1, mycontract, endDate, bRollOver, bSize, wTS, 1, 1, False, [])
+
+    time.sleep(3)
+    app.disconnect()
+    app.run()
+    
+    
     
 
 
